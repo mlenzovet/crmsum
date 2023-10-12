076f98dcd5a7c4afe5b9613c50e7acc9dfa5b164
Dimildizio <42382713+Dimildizio@users.noreply.github.com>
2023-10-13 05:43:38 +0800

    feat(dev): add regex for find_data_range

--- crmsum/find_date_range.py
+++ crmsum/find_date_range.py
@@ -1,19 +1,81 @@
-from datetime import datetime, timedelta
+"""
+Module for finding date ranges in text using regular expressions and date utilities.
+"""
+#  As LLM+Langchain are not implemented yet, it is plausible to utilize regex for dates filtering
 
-def find_date_range(text):
+import re
+from datetime import datetime, timedelta
+from typing import Tuple, List
+
+
+def get_re_dates(raw_text: str) -> List:
     """
-    Находит диапазон дат в тексте.
+    Использует регулярные выражения для поиска всех дат в формате YYYY-MM-DD.
 
     Аргументы:
-    text (str): Текст в свободной форме, в котором нужно найти диапазон дат. Например: "расскажи, что Ева делала с 2023-04-28 по 2023-05-05."
+      raw_text (str): Текст в свободной форме, в котором нужно найти диапазон дат.
+                  Например: "расскажи, что Ева делала с 2023-04-28 по 2023-05-05."
+
+    Возвращает:
+      dates (list): Список всех найденных в тексте дат в формате YYYY-MM-DD.
+    """
+    date_pattern = r'\d{4}-\d{2}-\d{2}'
+    dates = sorted(re.findall(date_pattern, raw_text))  # sorting might be unnecessary
+    return dates
+
+
+def get_current_date() -> str:
+    """
+    Возвращает текущую дату.
+    Возвращает:
+      dates (list): текущая дата в текстовом формате YYYY-MM-DD.
+    """
+    return datetime.now().strftime("%Y-%m-%d")
+
+
+def get_next_week_date() -> str:
+    """
+    Возвращает дату через неделю от текущей.
+    Возвращает:
+      dates (list): дата через неделю от текущей в текстовом формате YYYY-MM-DD.
+    """
+    return (datetime.now() + timedelta(days=7)).strftime("%Y-%m-%d")
+
+
+def find_date_range(text: str) -> Tuple[str, str]:
+    """
+    Находит диапазон дат в тексте.
+    Аргументы:
+    text (str): Текст в свободной форме, в котором нужно найти диапазон дат. Например: "расскажи, что Ева
+                делала с 2023-04-28 по 2023-05-05."
 
     Возвращает:
     start_date (str): Начальная дата диапазона в формате 'YYYY-MM-DD' или текущая дата, если дата не найдена.
-    end_date (str): Конечная дата диапазона в формате 'YYYY-MM-DD' или дата через неделю от текущей, если дата не найдена.
+    end_date (str): Конечная дата диапазона в формате 'YYYY-MM-DD' или дата через неделю от текущей,
+                    если дата не найдена.
     """
-    # Заглушка, добавьте ваш код здесь
-    start_date = datetime.now().strftime("%Y-%m-%d")
-    end_date = (datetime.now() + timedelta(days=7)).strftime("%Y-%m-%d")
+    # Edge cases: Unsorted date, single date, different format.
+    dates = get_re_dates(text)
 
-    # Вернуть найденный диапазон дат
+    if len(dates) >= 2:
+        start_date = dates[0]
+        end_date = dates[-1]
+    elif len(dates) == 1:
+        start_date = dates[0]
+        end_date = get_next_week_date()  # if no end date
+    else:
+        start_date = get_current_date()
+        end_date = get_next_week_date()
+
     return start_date, end_date
+
+
+if __name__ == '__main__':
+
+    texts = ("расскажи, что Ева делала с 2023-04-28 по 2023-05-05",
+             "расскажи, что Ева делала с 2024-04-28 по 2023-05-05",
+             "расскажи, что Ева делала 2023-04-28",
+             "расскажи, что Ева делала")
+    for entry in texts:
+        result = find_date_range(entry)
+        print(f"Ввод: {entry}:\nРезультат: {result}\n")
